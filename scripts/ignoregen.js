461ec5320b5b475d9ca1c705d8a70494f3637609
sakamossan <mild7caloriemategreentea@gmail.com>
2020-05-03 19:04:23 +0900

    fix npm-script prepare

--- scripts/ignoregen.js
+++ scripts/ignoregen.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const gitignore = fs.readFileSync('.gitignore').toString().split('\n');
+const gitignore = fs.readFileSync('.gitignore').toString().split('\n').filter(l => !l.match(/^dist\//))
 fs.writeFileSync('.npmignore', [
   ...gitignore,
   'default.code-workspace',
