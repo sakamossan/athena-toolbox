fe775e443fb4d5c90ba89537cbd836cc2828bd11
sakamossan <mild7caloriemategreentea@gmail.com>
2020-05-05 15:40:06 +0900

    fix scripts/ignoregen.js

--- scripts/ignoregen.js
+++ scripts/ignoregen.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const gitignore = fs.readFileSync('.gitignore').toString().split('\n').filter(l => !l.match(/^dist\//))
+const gitignore = fs
+  .readFileSync('.gitignore')
+  .toString()
+  .split('\n')
+  .filter(l => !l.match(/^dist\//))
 fs.writeFileSync('.npmignore', [
   ...gitignore,
   'default.code-workspace',
+  'scripts/ignoregen.js',
   'README.md',
   '__tests__/',
+  'dist/__tests__',
   'src/',
   'index.ts',
   'jest.config.js',
