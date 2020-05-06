ea490005cc477d6bbe9c000c1b55ae6e70fe6896
sakamossan <mild7caloriemategreentea@gmail.com>
2020-05-06 10:42:15 +0900

    export AthenaDatePartitioner

--- index.ts
+++ index.ts
@@ -4,10 +4,12 @@ import {
   generateHiveSymlinkTextOfCloudFrontAccessLog,
   listCloudFrontLoggingConfig,
 } from './src/HiveSymlinkTextInputGenerator';
+import { AthenaDatePartitioner } from './src/AthenaPartitioner';
 
 export {
   HiveSymlinkTextInputGenerator,
   CloudFrontClient,
+  AthenaDatePartitioner,
   generateHiveSymlinkTextOfCloudFrontAccessLog,
   listCloudFrontLoggingConfig,
 };
