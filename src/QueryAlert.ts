3688dd3f097ea74b6a4b8b5f2294284f8f12a01e
sakamossan <mild7caloriemategreentea@gmail.com>
2020-05-30 13:47:09 +0900

    listNamedQueries

--- src/QueryAlert.ts
+++ src/QueryAlert.ts
@@ -1,7 +1,7 @@
 import { Athena, S3 } from 'aws-sdk';
+import { default as pmap } from 'p-map';
 
-(async () => {
-  const athena = new Athena({ region: 'ap-northeast-1' });
+const listNamedQueries = async (athena: Athena) => {
   let namedQueryIds: string[] = [];
   let NextToken;
   let response;
@@ -10,4 +10,25 @@ import { Athena, S3 } from 'aws-sdk';
     namedQueryIds = namedQueryIds.concat(response.NamedQueryIds || []);
     NextToken = response.NextToken;
   } while (NextToken);
+
+  // We have to sleep more than documented to avoid getting stuck in the service quota.
+  // https://docs.aws.amazon.com/athena/latest/ug/service-limits.html#service-limits-api-calls
+  const concurrency = 2;
+  const namedQueries = await Promise.all(
+    await pmap(
+      namedQueryIds,
+      async (id: string) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return athena.getNamedQuery({ NamedQueryId: id }).promise();
+      },
+      { concurrency }
+    )
+  );
+  return namedQueries.flatMap(({ NamedQuery }) => NamedQuery || []);
+};
+
+(async () => {
+  const athena = new Athena({ region: 'ap-northeast-1' });
+  const namedQuerries = await listNamedQueries(athena);
+  console.log(JSON.stringify(namedQuerries));
 })();
